d5ac64d92f6620bcbe61fec7914e2b33d8d69d4b
Reza <rezadwiandrianto@gmail.com>
2023-06-27 19:11:51 +0700

    add Welcome Page

---
+++ src/Components/WelcomePage.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class WelcomePage extends Component {
+  render() {
+    return (
+      <div className="body-container">
+        <h1 className="title">Welcome to Sky Angel!</h1>
+        <button className="button">Start Game</button>
+      </div>
+    );
+  }
+}
+
+export default WelcomePage;
